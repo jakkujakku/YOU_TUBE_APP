4fb1e8b0c40353317d808f29f477559e991c9fdf
jakkujakku <jakkujakku98@gmail.com>
2023-09-04 16:47:18 +0900

    [FEAT]: 탭바 구현 부분 변경 후 콜렉션뷰 다시 추가

--- YOU_TUBE/Views/ThumbnailCell.swift
+++ YOU_TUBE/Views/ThumbnailCell.swift
@@ -9,6 +9,9 @@ import UIKit
 
 final class ThumbnailCell: UICollectionViewCell {
     static let identifier = "ThumbnailCell"
+<<<<<<< HEAD
 
     @IBOutlet weak var imageView: UIImageView!
+=======
+>>>>>>> 1ed9dae ([FEAT]: 탭바 구현 부분 변경 후 콜렉션뷰 다시 추가)
 }
