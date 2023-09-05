cf91b12430134c21b379a8cfa6897dc0e93e6e49
yk <youn9kun@gmail.com>
2023-09-05 20:48:36 +0900

    [MERGE]: copy 브랜치병합

--- YOU_TUBE/Controllers/LoginController.swift
+++ YOU_TUBE/Controllers/LoginController.swift
@@ -8,9 +8,38 @@
 import UIKit
 
 class LoginController: UIViewController {
+    @IBOutlet weak var youtubeLogo: UIImageView!
+    
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    @IBOutlet weak var loginbutton: UIButton!
+    
+    
+    @IBOutlet weak var loginbutton2: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
     }
+    
+    
+    @IBAction func loginbuttonTapped(_ sender: Any) {
+        
+        text1.placeholder = "이메일을 적으시오"
+    }
+    
+    
+    @IBAction func registerbuttonTapped(_ sender: Any) {
+    }
+    
 }
+
+
+
