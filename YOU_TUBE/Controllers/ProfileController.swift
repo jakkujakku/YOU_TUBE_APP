b07ad7b6b7d68d53d8b5d2cfda46fa0a135cad66
KangJiHun1028 <minsakr10@naver.com>
2023-09-05 19:07:02 +0900

    [FEAT]: 저장 버튼을 클릭 시 데이터 전달 기능 추가

--- YOU_TUBE/Controllers/ProfileController.swift
+++ YOU_TUBE/Controllers/ProfileController.swift
@@ -8,33 +8,76 @@
 import UIKit
 
 class ProfileController: UIViewController {
-    @IBOutlet weak var ProfileImageView: UIImageView!
+    @IBOutlet weak var profileImageView: UIImageView!
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        // 데이터 전달 받기
+        if let imageData = UserDefaults.standard.data(forKey: "Image") {
+            if let image = UIImage(data: imageData) {
+                profileImageView.image = image
+            }
+        }
+
+        if let name = UserDefaults.standard.string(forKey: "Name") {
+            nameTextField.text = name
+        }
+
+        if let email = UserDefaults.standard.string(forKey: "Email") {
+            emailTextField.text = email
+        }
+
+        // NotificationCenter로부터의 데이터 변경 알림 처리
+        NotificationCenter.default.addObserver(self, selector: #selector(handleProfileDataUpdated), name:
+            NSNotification.Name("ProfileDataUpdated"), object: nil)
     }
-    
-    @IBAction func Membership(_ sender: UIButton) {
+
+    @objc private func handleProfileDataUpdated() {
+        // 프로필 정보 업데이트 로직 (UserDefaults로부터 다시 불러옴)
+        if let imageData = UserDefaults.standard.data(forKey: "Image") {
+            if let image = UIImage(data: imageData) {
+                profileImageView.image = image
+            }
+        }
+
+        if let name = UserDefaults.standard.string(forKey: "Name") {
+            nameTextField.text = name
+        }
+
+        if let email = UserDefaults.standard.string(forKey: "Email") {
+            emailTextField.text = email
+        }
+    }
+
+    deinit {
+        // Notification Observer 해제 (메모리 누수 방지)
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @IBAction func memberShip(_ sender: UIButton) {
         // alert (제목, 메시지, alert종류) 지정
         let alert = UIAlertController(title: "회원삭제", message: "회원삭제 하시겠습니까?", preferredStyle: .alert)
         // 확인 버튼
         let confirm = UIAlertAction(title: "확인", style: .default, handler: nil)
         // 취소 버튼
         let close = UIAlertAction(title: "취소", style: .destructive, handler: nil)
-        
+
         alert.addAction(confirm)
         alert.addAction(close)
         present(alert, animated: true, completion: nil)
     }
-    
-    @IBAction func LogoutButton(_ sender: Any) {
+
+    @IBAction func logoutButton(_ sender: Any) {
         // alert (제목, 메시지, alert종류) 지정
         let alert = UIAlertController(title: "로그아웃", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
         // 확인 버튼
         let confirm = UIAlertAction(title: "확인", style: .default, handler: nil)
         // 취소 버튼
         let close = UIAlertAction(title: "취소", style: .destructive, handler: nil)
-        
+
         alert.addAction(confirm)
         alert.addAction(close)
         present(alert, animated: true, completion: nil)
--- YOU_TUBE/Controllers/ProfileEditViewController.swift
+++ YOU_TUBE/Controllers/ProfileEditViewController.swift
@@ -9,52 +9,66 @@ import UIKit
 
 class ProfileEditViewController: UIViewController {
     let picker = UIImagePickerController()
-
-    @IBOutlet weak var ImageView: UIImageView!
-    @IBOutlet var OpenPhotosButton: UIView!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var openPhotosButton: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         picker.delegate = self
     }
-
-    @IBAction func EditButton(_ sender: Any) {
+    
+    @IBAction func editButton(_ sender: Any) {
         let alert = UIAlertController(title: "프로필 수정", message: "프로필을 수정하시겠습니까?", preferredStyle: .actionSheet)
-
+        
         let library = UIAlertAction(title: "사진앨범", style: .default) { _ in self.openLibrary()
         }
-
+        
         let camera = UIAlertAction(title: "카메라", style: .default) { _ in
-
+            
             self.openCamera()
         }
-
+        
         let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-
+        
         alert.addAction(library)
-
+        
         alert.addAction(camera)
-
+        
         alert.addAction(cancel)
-
+        
         present(alert, animated: true, completion: nil)
     }
-
+    
     func openLibrary() {
         picker.sourceType = .photoLibrary
-
+        
         present(picker, animated: false, completion: nil)
     }
-
+    
     func openCamera() {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             picker.sourceType = .camera
-
+            
             present(picker, animated: false, completion: nil)
         }
-
+        
         else {
             print("Camera not available")
         }
+    }
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        // 데이터 전달
+        UserDefaults.standard.set(imageView.image?.pngData(), forKey: "Image")
+        UserDefaults.standard.set(nameTextField.text, forKey: "Name")
+        UserDefaults.standard.set(emailTextField.text, forKey: "Email")
+            
+        // NotificationCenter를 사용하여 데이터 전달
+        NotificationCenter.default.post(name: NSNotification.Name("ProfileDataUpdated"), object: nil)
+            
+        navigationController?.popViewController(animated: true)
     }
 }
 
@@ -62,7 +76,7 @@ extension ProfileEditViewController: UIImagePickerControllerDelegate, UINavigati
     // 이미지 선택 및 촬영 완료 후 호출되는 메서드 구현
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         if let image = info[.originalImage] as? UIImage {
-            ImageView?.image = image
+            imageView?.image = image
         }
 
         dismiss(animated: true, completion: nil)
