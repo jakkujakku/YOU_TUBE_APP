ab56c0757d029c8fa3043283ecd3033d4bfcf57d
KangJiHun1028 <minsakr10@naver.com>
2023-09-08 14:41:42 +0900

    [FIX]: TableView Cell, ImageView 정렬

--- YOU_TUBE/Views/ProfileCell.swift
+++ YOU_TUBE/Views/ProfileCell.swift
@@ -10,6 +10,17 @@ import UIKit
 class ProfileCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var profileLabel: UILabel!
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        // 이미지와 텍스트의 정렬 설정
+        profileImageView.contentMode = .scaleAspectFit // 이미지를 셀 내에서 비율을 유지하며 맞춤
+        profileImageView.clipsToBounds = true // 이미지가 셀을 넘어갈 경우 잘라냄
+
+        profileLabel.textAlignment = .left // 텍스트를 왼쪽으로 정렬
+        profileLabel.font = UIFont.systemFont(ofSize: 16) // 텍스트의 폰트 설정 (원하는 폰트 및 크기로 변경)
+    }
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
